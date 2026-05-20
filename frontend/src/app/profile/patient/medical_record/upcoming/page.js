2388f4b791ce2bd627c81d6b084655b95dc64d57
trieu123x <trieudh14@gmail.com>
2026-05-21 00:53:11 +0700

    fix pack

--- frontend/src/app/profile/patient/medical_record/upcoming/page.js
+++ frontend/src/app/profile/patient/medical_record/upcoming/page.js
@@ -2,7 +2,7 @@
 
 import { useState, useEffect, useMemo } from "react";
 import Image from "next/image";
-import FilterImage from "../../../../../../public/images/Filter.svg";
+import FilterImage from "@/public/images/Filter.svg";
 import { UpcomingAppointmentItem } from "../../../../../components/medicalRecord/upcomingItem"; 
 import { appointmentApi } from "@/routers/appointment/appointmentRouter";
 import { ConfirmModal } from "@/components/ui/Modal"; 
